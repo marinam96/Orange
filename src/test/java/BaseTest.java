94b9e75ac879659ae6f2bfb343bc9fa451d6c434
marinam96 <marinam96@hotmail.rs>
2023-03-30 19:56:53 +0200

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    public ChromeDriver openBrowser()
+    {
+    ChromeDriver driver =new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+    driver.navigate().to("")
+
+    }
+}
+
